d1632e94b885ebd0040cc570275654fc7f3b2383
Ayrat <joevegojv@gmail.com>
2023-12-14 16:34:31 +0300

    Creating OOP structure p2

--- src/log/WandBLog.py
+++ src/log/WandBLog.py
@@ -4,3 +4,6 @@ class WandBLog():
     def __init__(self, model):
         wandb.init()
         wandb.watch(model)
+
+    def getWandB(self):
+        return wandb
--- src/main.py
+++ src/main.py
@@ -1,14 +1,83 @@
 import torch
+import numpy
+from tqdm import tqdm
+from torch import nn
 
 from src.datasets.Data_set import Data_set as dataset
 from src.device.Using_device import get_device
 from src.dataloaders.TestSampler import TestSampler
 from src.net.EarthClassNet import EarthClastNet
 from src.log.WandBLog import WandBLog
+from src.PictToClass import pic_to_class
 
 loss_func = torch.nn.CrossEntropyLoss()
-def setOptimizer(model):
-    return torch.optim.SGD(model.parameters(), lr = 0.01)
+NUM_OF_EPOCH = 3
+
+
+def getOptimizer(model, learning_rate=0.01):
+    """устанавливаем оптимайзер"""
+    return torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+
+def train(model, optimizer, train_dataloader):
+    """Обучение модели"""
+
+    # берем пример и ответ
+    for sample_train, answer_train in tqdm(train_dataloader):
+        # переносим батчи на девайс
+        sample_train, answer_train = sample_train.to(get_device()), answer_train.to(get_device())
+        print("Размерность батча трейн пример ", sample_train.shape)
+        print("Размерность батча трейн ответ  ", answer_train.shape)
+
+        # делаем предсказание и переносим его на девайс
+        prediction = model(sample_train).to(get_device())
+        # prediction = prediction.to(get_device())
+
+        print("\nответ сетки\n[bs, числа которые станут вероятностями]")
+        print(prediction.shape)
+        print(prediction)
+        # print("\nответ из выборки")
+        # print(y_train.shape)
+        # print("\nSoftMax по ответу: ")
+        # print(nn.Softmax()(y_pred))
+
+        # получаем классы по изображениям
+        answer_train = pic_to_class(answer_train)
+
+        loss = loss_func()(prediction, answer_train)
+        # loss =  torch.nn.NLLLoss()(torch.log(y_pred), y_train) - это было NLLL(Log(Softmax))
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+
+def validate(model, test_dataloader, wandb, mean_val_loss):
+
+    for sample_test, answer_test in tqdm(test_dataloader):
+        sample_test, answer_test = sample_test.to(get_device()), answer_test.to(get_device())
+        print("Размерность батча тест пример ", sample_test.shape)
+        print("Размерность батча тест ответ  ", answer_test.shape)
+
+        print("\nответ из выборки")
+        classes = pic_to_class(answer_test)
+        print(classes)
+
+        # y_pred2 = earth_model(x_val.permute(0, 3, 1, 2))
+        test_prediction = model(sample_test)
+        print("test_prediction shape = ", test_prediction.shape)
+        print("y_pred2 Ответ на выборку = ", test_prediction)
+        print("\nSoftMax по ответу: ")
+        print(nn.Softmax()(test_prediction))
+
+        loss = loss()(test_prediction, classes)
+        loss = loss.to(get_device())
+        mean_val_loss.append(loss.numpy())
+
+        # val_accuracy.extend((torch.argmax(y_pred2, dim=-1) == picToProbsTensor(y_val)).numpy().tolist())
+        print("mean_val_loss = ", numpy.mean(mean_val_loss))
+        wandb.log({"mean val loss:": numpy.mean(mean_val_loss)})
+
 
 if __name__ == '__main__':
     current_device = get_device()
@@ -49,7 +118,14 @@ if __name__ == '__main__':
     earth_model.to(get_device())
 
     # задаем оптимайзер
-    optimizer = setOptimizer(earth_model)
+    optimizer = getOptimizer(earth_model)
 
-    #инициализация лога
-    log = WandBLog(earth_model)
+    # инициализация лога
+    wandb = WandBLog(earth_model)
+
+    for epoch in range(NUM_OF_EPOCH):
+        train(earth_model, optimizer, train_dataloader)
+
+        mean_val_loss = []
+        with torch.no_grad():
+            validate(earth_model, test_dataloader, wandb.getWandB(), mean_val_loss)
